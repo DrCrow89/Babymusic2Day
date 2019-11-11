17abc49c90dc43eb1103b781333ced87f8533e9b
Roland Kuenen <rolandkuenen@gmx.net>
2019-11-11 22:43:04 +0100

    control musicplayer with inputs in new console

--- babymusic2Day.py
+++ babymusic2Day.py
@@ -1,67 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 import RPi.GPIO as GPIO
-import sys, time, threading
+import os, sys, time, threading, subprocess
 sys.path.append('MFRC522-python')
-sys.path.append('pygame')
-import MFRC522, pygame
-import signal
-
+import MFRC522
 '''---------------------- Konstanten ----------------------'''
 '''--------------------------------------------------------'''
-ZYKLUSZEIT_MAIN = 1.0 #Zykluszeit des Programms sind 100ms
+ZYKLUSZEIT_MAIN = 0.1 #Zykluszeit des Programms sind 100ms
 '''---------------------- Variablen -----------------------'''
 '''--------------------------------------------------------'''
 continue_reading = True
 letzte_uid = "LEER"
 
-def MFRC522_Reader(name):
-    t = threading.currentThread()
-    MIFAREReader = MFRC522.MFRC522() # Create an object of the class MFRC522
+def main():
     global continue_reading
     global letzte_uid
-    while continue_reading:
-        (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL) # Scan for card
-        (status,uid) = MIFAREReader.MFRC522_Anticoll() # Get the UID of the card
-        if status == MIFAREReader.MI_OK: # If we have the UID, continue
-            var = (str(uid[0]) + str(uid[1]) + str(uid[2]) + str(uid[3]))
-#            print "Aktuelle Kartennummer: "+ str(var)
-            if ((var == "185100155153")and(letzte_uid != "185100155153")):
-                letzte_uid = "185100155153"
-                print letzte_uid
-            elif ((var == "12157159163")and(letzte_uid != "12157159163")):
-                letzte_uid = "12157159163"
-                print letzte_uid
-
-def Musikplayer(name):
-    t = threading.currentThread()
-    pygame.mixer.init()
-    pygame.mixer.music.load("numb.mp3")
-    pygame.mixer.music.play()
-    print "play musik"
-    while continue_reading:
-        continue
-    pygame.mixer.music.stop()
-
-def main():
+    MIFAREReader = MFRC522.MFRC522() # Create an object of the class MFRC522
+    #musicplayer = subprocess.Popen(['mpg321', '-K', '-v', 'numb.mp3'], stdout=subprocess.PIPE)
+    musicplayer = subprocess.Popen(['mpg321', '-K', 'numb.mp3'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     try:
-        t_reader = threading.Thread(target=MFRC522_Reader, args=("Reader",))
-        t_player = threading.Thread(target=Musikplayer, args=("Musikplayer",))
-        t_reader.start()
-        t_player.start()
-        while True:
+        while continue_reading:
+            (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL) # Scan for card
+            (status,uid) = MIFAREReader.MFRC522_Anticoll() # Get the UID of the card
+            if status == MIFAREReader.MI_OK: # If we have the UID, continue
+                var = (str(uid[0]) + str(uid[1]) + str(uid[2]) + str(uid[3]))
+                if ((var == "185100155153")and(letzte_uid != "185100155153")):
+                    letzte_uid = "185100155153"
+                    print letzte_uid
+                elif ((var == "12157159163")and(letzte_uid != "12157159163")):
+                    letzte_uid = "12157159163"
+                    musicplayer.stdin.write('\0x03')
+                    musicplayer.stdin.flush()
+                    print letzte_uid
+            #print "Startausgabe"
+            musicplayer.stdin.write('/')
+            musicplayer.stdin.flush()
+            #print "Endausgabe\n"
             time.sleep(ZYKLUSZEIT_MAIN)
-#           print "Zeit"
 
     except KeyboardInterrupt:
-        global continue_reading
+        musicplayer.stdin.close()
+        musicplayer.terminate()
+        #print musicplayer.stdout.readline()
         continue_reading = False
         GPIO.cleanup()
-        t_reader.do_run = False
-        t_player.do_run = False
-        t_reader.join()
-        t_player.join()
         print "Programm beendet"
+    except:
+        musicplayer.stdin.close()
+        musicplayer.terminate()
+        #print musicplayer.stdout.readline()
+        continue_reading = False
+        GPIO.cleanup()
+        print "Programmfehler: " + str(sys.exc_info()[0])
 
 if __name__ == "__main__":
     main()
