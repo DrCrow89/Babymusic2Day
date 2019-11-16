e7154fb1b4810c6a452db51cec760eedc685d8ec
Roland Kuenen <rolandkuenen@gmx.net>
2019-11-16 13:10:10 +0100

    Prüfen auf ein vorhandenes Verzeichnis mit der eingelesenen uidn.

--- babymusic2Day.py
+++ babymusic2Day.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 import RPi.GPIO as GPIO
-import sys, time, threading
+import os, sys, time, threading
 sys.path.append('MFRC522-python')
 sys.path.append('pygame')
 import MFRC522, pygame
-import signal
 
 '''---------------------- Konstanten ----------------------'''
 '''--------------------------------------------------------'''
 ZYKLUSZEIT_MAIN = 0.2 #Zykluszeit des Programms
+VERZEICHNIS_DATEN = "./data"
 '''---------------------- Variablen -----------------------'''
 '''--------------------------------------------------------'''
 program_run = True
 letzte_uid = "LEER"
 
+def check_verzeichnis(ue_uid):
+    if (ue_uid != "LEER"): # Für die erste LOOP bei Programmstart nicht das Verzeichnis prüfen.
+        if os.path.isdir(os.path.join(VERZEICHNIS_DATEN, ue_uid)) == True: # Verzeichnis existiert
+            print "Verzeichnis: " + str(ue_uid) + " existiert"
+        else: # Verzeichnis existiert nicht
+            print os.path.join(VERZEICHNIS_DATEN, ue_uid)
+            print "Verzeichnis: " + str(ue_uid) + " existiert nicht"
+
+
 def read_chip(MIFAREReader):
+    global letzte_uid
+    ret_uid = letzte_uid
+    ret_neu = False
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL) # Scan for card
     (status,uid) = MIFAREReader.MFRC522_Anticoll() # Get the UID of the card
+    #print "Status: " + str(status)
     if status == MIFAREReader.MI_OK: # If we have the UID, continue
-        var = (str(uid[0]) + str(uid[1]) + str(uid[2]) + str(uid[3]))
-        print var
+        gelesene_uid = (str(uid[0]) + str(uid[1]) + str(uid[2]) + str(uid[3]))
+        if (gelesene_uid != ret_uid):
+            letzte_uid = gelesene_uid
+            ret_uid = gelesene_uid
+            ret_neu = True
+    return ret_neu, ret_uid
 
 def main():
     global letzte_uid
@@ -28,8 +45,15 @@ def main():
     MIFAREReader = MFRC522.MFRC522() # Create an object of the class MFRC522
     try:
         while program_run:
-            read_chip(MIFAREReader)
+            temp_status, temp_uid = read_chip(MIFAREReader) # temp_status is true if a new rfid chip is detected
+            if temp_status == True:
+                check_verzeichnis(temp_uid)
+            else:
+                # Keine neue Chipkarte erkannt
+                pass
+
             time.sleep(ZYKLUSZEIT_MAIN)
+            #print "ENDE"
 
     except KeyboardInterrupt:
         GPIO.cleanup()
