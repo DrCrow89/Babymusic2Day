588fa2d23c3c7b73acfb80d42ff477eefc9349f1
Roland Kuenen <rolandkuenen@gmx.net>
2020-01-12 00:42:38 +0100

    Umbau der GPIO pins und einlesen der Schalter für das ausschalten des Lichtes und der Lautstärke.

--- test.py
+++ test.py
@@ -4,28 +4,63 @@ import threading
 import time
 import os
 import RPi.GPIO as GPIO
+'''--------------------------------------------------------'''
 '''---------------------- Konstanten ----------------------'''
-ZYKLUSZEIT_MAIN = 0.1 # Zykluszeit des Programms sind 100ms
-ZYKLUSZEIT_ALIVE = 0.9 # Zykluszeit für das senden des Alive Flags
-GPIO_PIN_ALIVE = 11 # Pi Alive Flag für Energie-Kontroller
-GPIO_PIN_SHUTDOWN = 13 # Input Pin zum herunterfahren des Pi
-GPIO_PIN_LAUTER = 29 # Input Pin für lauter - Button
-GPIO_PIN_LEISER = 31 # Input Pin für leiser - Button
+ZYKLUSZEIT_MAIN = 0.1     # Zykluszeit des Programms sind 100ms
+ZYKLUSZEIT_ALIVE = 0.9    # Zykluszeit für das senden des Alive Flags
+GPIO_PIN_ALIVE = 7        # Pi Alive Flag für Energie-Kontroller
+GPIO_PIN_SHUTDOWN = 11    # Input Pin zum herunterfahren des Pi
+GPIO_PIN_BUTTON_MUTE = 13 # Input Pin Lautstärketasten aus- oder einschalten
+GPIO_PIN_LIGHT_MUTE = 15  # Input Pin zum ein- und ausschalten der Beleuchtung
+GPIO_PIN_LAUTER = 29      # Input Pin für lauter - Button
+GPIO_PIN_LEISER = 31      # Input Pin für leiser - Button
+'''--------------------------------------------------------'''
+'''---------------------- Variablen -----------------------'''
+global switch_button_mute # Variable zum einlesen des Schalters um die Lautstärketasten einzulesen
+global switch_light_mute  # Variable zum einlesen des Schalters um die Lichtsteuerung ein- oder auszuschalten
 '''--------------------------------------------------------'''
 '''------------------ GPIO Einstellungen ------------------'''
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(GPIO_PIN_ALIVE, GPIO.OUT)
 GPIO.setup(GPIO_PIN_SHUTDOWN, GPIO.IN)
+GPIO.setup(GPIO_PIN_BUTTON_MUTE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(GPIO_PIN_LIGHT_MUTE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(GPIO_PIN_LAUTER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(GPIO_PIN_LEISER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 '''--------------------------------------------------------'''
 '''------------------ Support Funktionen ------------------'''
 def increase_volume(ue_gpio_nummer):
-    print "Musik lauter"
+    global switch_button_mute
+    if switch_button_mute == True:
+        print "Musik lauter"
+    else:
+        print "Musik lauter ist ausgeschaltet"
 
 def decrease_volume(ue_gpio_nummer):
-    print "Musik leiser"
+    global switch_button_mute
+    if switch_button_mute == True:
+        print "Musik leiser"
+    else:
+        print "Musik leiser ist ausgeschaltet"
+
+def mute_volume_button(ue_gpio_nummer):
+    global switch_button_mute
+    if switch_button_mute == True:
+        switch_button_mute = False
+        print "Schalter für Lautstärketasten aus"
+    else:
+        switch_button_mute = True
+        print "Schalter für Lautstärketasten ein"
+
+def mute_light(ue_gpio_nummer):
+    global switch_light_mute
+    if switch_light_mute == True:
+        switch_light_mute = False
+        print "Schalter für Lichtsteuerung war aus"
+    else:
+        switch_light_mute = True
+        print "Schalter für Lichtsteuerung war ein"
 
 '''--------------------------------------------------------'''
 '''------------------- Thread Funktionen ------------------'''
@@ -44,8 +79,25 @@ def FlagPiIsAlive(name):
 '''--------------------------------------------------------'''
 '''--------------------- Hauptprogramm --------------------'''
 def main():
+    global switch_button_mute
+    global switch_light_mute
+    if GPIO.input(GPIO_PIN_BUTTON_MUTE) == True:
+        switch_button_mute = True
+        print "Schalter für Lautstärketasten war an"
+    else:
+        switch_button_mute = False
+        print "Schalter für Lautstärketasten war aus"
+    if GPIO.input(GPIO_PIN_LIGHT_MUTE) == True:
+        switch_light_mute = True
+        print "Schalter für Lichtsteuerung war an"
+    else:
+        switch_light_mute = False
+        print "Schalter für Lichtsteuerung war aus"
+
     GPIO.add_event_detect(GPIO_PIN_LAUTER,GPIO.RISING,callback=increase_volume,bouncetime=800)
     GPIO.add_event_detect(GPIO_PIN_LEISER,GPIO.RISING,callback=decrease_volume,bouncetime=800)
+    GPIO.add_event_detect(GPIO_PIN_BUTTON_MUTE,GPIO.BOTH,callback=mute_volume_button,bouncetime=100)
+    GPIO.add_event_detect(GPIO_PIN_LIGHT_MUTE,GPIO.BOTH,callback=mute_light,bouncetime=100)
 
     try:
         t_pi_alive = threading.Thread(target=FlagPiIsAlive, args=("Pi is alive",))
