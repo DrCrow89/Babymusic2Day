6a0c1dae772110f1fdb4d8ae51d98f265a716e14
Roland Kuenen <rolandkuenen@gmx.net>
2020-03-27 14:53:22 +0100

    Anpassen der Funktion zum zurückschreiben in den Log, beim Stoppen des Abspielens.

--- babymusic2Day.py
+++ babymusic2Day.py
@@ -80,23 +80,26 @@ def start_musikplayer(ue_aktuelle_playliste, ue_aktueller_titel, ue_start):
     # Testen ob es den aktuellen Startpunkt überhaupt gibt.
     pygame.mixer.music.play(start=ue_start)
 
-def set_musikdaten(ue_path, ue_section, ue_option_titel, ue_option_stelle):
+def set_musikdaten(ue_path, ue_section, ue_option_titel, ue_option_stelle, ue_option_spielzeit):
     erfolgreich_gelesen, temp_zaehler = config2Day.get_value_int(ue_path, "Log", "zaehler_abgespielt") # Funktion wird nur aufgerufen, wenn der Musikplayer im normalen Betrieb gestoppt wird durch abnehmen der Figur
+    erfolgreich_gelesen, temp_spielzeit = config2Day.get_value_int(ue_path, "Log", "gesamt_spielzeit")
     temp_zaehler = temp_zaehler + 1
+    temp_spielzeit = temp_spielzeit + ue_option_spielzeit
     config2Day.set_value(ue_path, "Log", "zaehler_abgespielt", str(temp_zaehler))
     config2Day.set_value(ue_path, "Log", "letzter_titel", str(ue_option_titel))
     config2Day.set_value(ue_path, "Log", "letzte_stelle", str(ue_option_stelle))
+    config2Day.set_value(ue_path, "Log", "gesamt_spielzeit", str(temp_spielzeit))
 
 def stop_musikplayer_hart():
     if pygame.mixer.music.get_busy() == True:
         pygame.mixer.music.stop()
 
-def stop_musikplayer(ue_path, ue_titel, ue_spielzeit_offset):
+def stop_musikplayer(ue_path, ue_titel, ue_spielzeit_offset, ue_gesamtspielzeit):
     if pygame.mixer.music.get_busy() == True:
         if DEBUG_MODE == True:
             print "Speichern"
         speicherzeit = (((pygame.mixer.music.get_pos())/1000)+ue_spielzeit_offset) # Die aktuelle Spielzeit richtet sich nur nach dem Playerstart. Nicht nach dem Musikstart und muss daher aufaddiert werden.
-        set_musikdaten(ue_path, "Log", ue_titel, speicherzeit)
+        set_musikdaten(ue_path, "Log", ue_titel, speicherzeit, ue_gesamtspielzeit)
         pygame.mixer.music.stop()
 
 def increase_volume(ue_gpio_nummer):
@@ -248,7 +251,8 @@ def main():
     global aktueller_titel_index
     MIFAREReader = MFRC522.MFRC522() # Create an object of the class MFRC522
     chip_auf_leser = 0
-    spielzeit_offset = 0
+    spielzeit_offset = 0 # Um bei Hörspielen die richtige Startzeit zu erfassen und zu speichern
+    start_gesamt_spielzeit = 0 # Startzeit des Abspielens um die Gesamtspielzeit zu ermitteln
 
     read_config_switch()
     GPIO.add_event_detect(GPIO_PIN_LAUTER,GPIO.RISING,callback=increase_volume,bouncetime=800)
@@ -273,7 +277,7 @@ def main():
                 if (chip_uid == "LEER"):
                     stop_musikplayer_hart()
                 else:
-                    stop_musikplayer(os.path.join(aktuelles_musik_verzeichnis, NAME_LOG_DATEI), aktueller_titel, spielzeit_offset)
+                    stop_musikplayer(os.path.join(aktuelles_musik_verzeichnis, NAME_LOG_DATEI), aktueller_titel, spielzeit_offset, time.time()-start_gesamt_spielzeit)
 
                 aktuelles_musik_verzeichnis = os.path.join(VERZEICHNIS_DATEN, chip_uid)
                 erfolgreich_gelesen, musiktyp = config2Day.get_value(os.path.join(VERZEICHNIS_DATEN, chip_uid, NAME_LOG_DATEI), "Grundeinstellung", "Typ")
@@ -299,13 +303,14 @@ def main():
                 else:
                     pass
                 start_musikplayer(aktuelle_playliste, aktueller_titel, spielzeit_offset)
+                start_gesamt_spielzeit = time.time()
             elif ((neue_uid == True)and(check_verzeichnis(chip_uid)) == False):
                 # Neuer Chip auf leser aber keine Musik vorhanden. Musikplayer stoppen.
-                stop_musikplayer(os.path.join(aktuelles_musik_verzeichnis, NAME_LOG_DATEI), aktueller_titel, spielzeit_offset)
+                stop_musikplayer(os.path.join(aktuelles_musik_verzeichnis, NAME_LOG_DATEI), aktueller_titel, spielzeit_offset, time.time()-start_gesamt_spielzeit)
 
             elif (chip_uid == "LEER"):
                 # Kein Chip mehr vorhanden, stoppe Musik
-                stop_musikplayer(os.path.join(aktuelles_musik_verzeichnis, NAME_LOG_DATEI), aktueller_titel, spielzeit_offset)
+                stop_musikplayer(os.path.join(aktuelles_musik_verzeichnis, NAME_LOG_DATEI), aktueller_titel, spielzeit_offset, time.time()-start_gesamt_spielzeit)
             else:
                 # Keine Veränderung und eine gültige Musikwiedergabe
                 pass
@@ -320,6 +325,7 @@ def main():
                     print "Nächster Titel"
                 aktueller_titel = aktuelle_playliste[aktueller_titel_index]
                 start_musikplayer(aktuelle_playliste, aktuelle_playliste[aktueller_titel_index], 0)
+                start_gesamt_spielzeit = time.time()
             else:
                 pass
 
@@ -327,7 +333,7 @@ def main():
             time.sleep(ZYKLUSZEIT_MAIN)
 
         print "Programm beenden"
-        stop_musikplayer(os.path.join(aktuelles_musik_verzeichnis, NAME_LOG_DATEI), aktueller_titel, spielzeit_offset)
+        stop_musikplayer(os.path.join(aktuelles_musik_verzeichnis, NAME_LOG_DATEI), aktueller_titel, spielzeit_offset, time.time()-start_gesamt_spielzeit)
         t_pi_alive.do_run = False
         t_pi_alive.join()
         GPIO.cleanup()
